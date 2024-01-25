2997dd3ed8352d4a90bf9124f9f24cbb40a896e0
nishy02 <146426479+nishy02@users.noreply.github.com>
2024-01-25 19:26:05 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=143
+let accountEmail="hello@123"
+var accountPass="1234"
+let accountNO
+accountCity="Jamshedpur"
+accountEmail="New"
+accountPass="786"
+accountCity="kolkata"
+console.table([accountId,accountEmail,accountPass,accountCity,accountNO])
